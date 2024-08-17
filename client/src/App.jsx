346572fde5c561f227dc5b641a37410a2f79823a
Tejas Yadav <tejasy919@gmail.com>
2024-08-17 12:02:17 +0530

    3rd commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Users from './Users'
+import Users from './USers'
 import CreateUser from './createUser'
 import UpdateUser from './updateUser'
 
--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -7,27 +7,30 @@ const Users = () => {
     }])
   return (
     <div className='flex h-full bg-pink-200 justify-center align-middle'>
-      <div className='w-48 bg-white rounded-2xl p-3'>
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Age</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-               { users.map((user) => {
-                    <tr>
-                        <td>{user.Name}</td>
-                        <td>{user.Email}</td>
-                        <td>{user.Age}</td>
-                        <td><button>Edit</button><button>Delete</button></td>
-                    </tr>
-                })}
-            </tbody>
-        </table>
+      <div className=' w-100 bg-white rounded-2xl p-3 '>
+        <div className='flex bg-green-400 flex-auto justify-between mx-3'>
+          <h1 className='text-xl bg-yellow-200 font-semibold text-gray-950 '>
+            Card
+          </h1>
+          <div className='grid grid-flow-col grid-cols-2 grid-rows-4 m-3 p-3 pt-1'>
+            <div className='flex justify-between items-center font-semibold'>
+              <h1>Name</h1>
+              <h1>Email</h1>
+              <h1>Age</h1>
+              
+            </div>
+            {users.map((user)=>{
+              return(
+                <div className='border-y-2 w-full py-3 justify-between items-center'>
+                  <h1>{user.Name}</h1>
+                  <h1>{user.Email}</h1>
+                  <h1>{user.Age}</h1>
+                </div>
+              )
+            })}
+
+          </div>
+        </div>
       </div>
     </div>
   )
